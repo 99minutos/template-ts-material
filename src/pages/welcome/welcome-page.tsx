10123bd6a0c05e78c3f046f8822d2ecd1281deb5
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2025-06-05 09:54:48 -0700

    feat: add sections

--- src/pages/welcome/welcome-page.tsx
+++ src/pages/welcome/welcome-page.tsx
@@ -1,8 +1,14 @@
 import { Box, TextField } from '@mui/material';
 
+import WelcomeSection from '@/sections/welcome/welcome-section';
+
 export default function WelcomePage() {
   return (
     <Box className="py-4">
+      <div>
+        <WelcomeSection />
+      </div>
+
       <div>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </div>
