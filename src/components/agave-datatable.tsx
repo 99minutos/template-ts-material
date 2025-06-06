8ad3ce0ffc9669f577b72f3ae4a17e1933145ca3
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2025-06-06 09:20:06 -0700

    feat: upgrade datatable, add date-fns

--- src/components/agave-datatable.tsx
+++ src/components/agave-datatable.tsx
@@ -6,11 +6,10 @@ import {
   gridPageCountSelector,
   gridPageSelector,
   gridPageSizeSelector,
-  GridRowSpacingParams,
   useGridApiContext,
 } from '@mui/x-data-grid';
 
-import BootstrapInput from './BootstrapInput';
+import BootstrapInput from './bootstrap-input';
 
 export const AgavePagination = () => {
   const apiRef = useGridApiContext();
@@ -62,10 +61,12 @@ const AgaveDataGrid = styled(DataGrid)<DataGridProps>(({ theme }) => ({
   },
   '& .MuiDataGrid-virtualScrollerRenderZone': {
     height: '100%',
+    position: 'relative',
   },
   '& .MuiDataGrid-row': {
-    // userSelect: 'none',
     borderRadius: 8,
+    marginTop: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
     width: 'calc(100% - 2px)',
     border: '1px solid rgba(224, 224, 224, 1)',
     backgroundColor: theme.palette.common.white,
@@ -73,16 +74,17 @@ const AgaveDataGrid = styled(DataGrid)<DataGridProps>(({ theme }) => ({
   '& .MuiDataGrid-cell': {
     border: 'none',
     position: 'relative',
+    color: 'black',
   },
-  '& .MuiDataGrid-cell:not(:last-child):after': {
-    right: 1,
+  '& .MuiDataGrid-row .MuiDataGrid-cell:not(:last-of-type):after': {
+    right: 0,
     width: 1,
     zIndex: 2,
     content: "''",
     position: 'absolute',
     backgroundColor: grey[300],
-    top: theme.spacing(1),
-    bottom: theme.spacing(1),
+    top: theme.spacing(1.5),
+    bottom: theme.spacing(1.5),
   },
   '& .MuiDataGrid-footerContainer': {
     border: 'none',
@@ -90,29 +92,4 @@ const AgaveDataGrid = styled(DataGrid)<DataGridProps>(({ theme }) => ({
     justifyContent: 'center !important',
   },
 }));
-
-AgaveDataGrid.defaultProps = {
-  getRowSpacing: (params: GridRowSpacingParams) => ({
-    top: params.isFirstVisible ? 4 : 4,
-    bottom: params.isLastVisible ? 0 : 4,
-  }),
-  paginationModel: {
-    pageSize: 10,
-    page: 0,
-  },
-  rowHeight: 76,
-  pageSizeOptions: [10, 25, 50, 75, 100],
-  slots: {
-    // pagination: AgavePagination,
-  },
-  localeText: {
-    noRowsLabel: 'No hay filas',
-    MuiTablePagination: {
-      labelRowsPerPage: 'Filas por página',
-      labelDisplayedRows: ({ from, to, count }) =>
-        `${from}-${to} de ${count !== -1 ? count : `más de ${to}`}`,
-    },
-  },
-};
-
 export { AgaveDataGrid };
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import '@/styles/global.css';
 
 import { RouteManager } from '@/router-manager';
 import { ThemeProvider } from '@mui/material/styles';
+import { setDefaultOptions } from 'date-fns';
+import { es } from 'date-fns/locale';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import theme from '@/styles/theme';
 
 import AuthProvider from './components/providers/auth/auth-provider';
+
+setDefaultOptions({ locale: es });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -18,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
         <AuthProvider>
           <RouteManager />
         </AuthProvider>
+        <ToastContainer position="top-right" autoClose={5000} closeOnClick pauseOnHover />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
